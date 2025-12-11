6a04e6f6d1f92242caae20e04d968780b3e9baec
Andreal <71481916+andrealteam@users.noreply.github.com>
2025-12-11 11:50:11 +0530

    Link change with v2

--- src/pages/FinalReview.jsx
+++ src/pages/FinalReview.jsx
@@ -30,7 +30,7 @@ const FinalReview = () => {
         <p>
           In the meantime, you can learn more about us by visiting the <br />
           <a
-            href="https://eepc-mern-frontend.vercel.app/dashboard/exporter-home-page"
+            href="https://eepc-mern-frontend-v2.vercel.app/dashboard/exporter-home-page"
             target="_blank"
             rel="noopener noreferrer"
           >
