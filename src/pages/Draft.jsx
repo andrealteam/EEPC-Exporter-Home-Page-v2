26d1804d739305037bbce7f4134152ccba2361aa
Andreal <71481916+andrealteam@users.noreply.github.com>
2025-12-15 18:10:55 +0530

    update change

--- src/pages/Draft.jsx
+++ src/pages/Draft.jsx
@@ -449,7 +449,7 @@ const Draft = () => {
             onClick={handleLoginClick}
             style={{
               position: "fixed",
-              top: "150px",
+              top: "20px",
               right: "20px",
               zIndex: 10000,
               padding: "10px 20px",
