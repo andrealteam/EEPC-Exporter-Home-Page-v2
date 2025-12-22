5ee6441c332d1f95c10fd84be006914962e93f8a
Andreal <71481916+andrealteam@users.noreply.github.com>
2025-12-22 21:07:44 +0530

    undo changes

--- src/components/live/BannerLive.jsx
+++ src/components/live/BannerLive.jsx
@@ -517,44 +517,42 @@ function BannerLive({ website_url, isAdmin, member_id }) {
                   </button>
                 )}
 
-              {!member_id && (
-                <>
-                  <button className="button" onClick={addFavoriteWithoutModal}>
-                    {isFavorite === "added" ? (
-                      <span>❤️ Added to Favorite</span>
-                    ) : (
-                      <span>🤍 Add to Favorite</span>
-                    )}
-                  </button>
-                  <button
-                    className="button"
-                    onClick={() => setIsReviewModalOpen(true)}
-                    style={{
-                      display: "flex",
-                      alignItems: "center",
-                      gap: "5px",
-                    }}
-                  >
-                    <div className="svg-wrapper-1">
-                      <div className="svg-wrapper">
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          viewBox="0 0 24 24"
-                          width="24"
-                          height="24"
-                        >
-                          <path fill="none" d="M0 0h24v24H0z"></path>
-                          <path
-                            fill="currentColor"
-                            d="M1.946 9.315c-.522-.174-.527-.455.01-.634l19.087-6.362c.529-.176.832.12.684.638l-5.454 19.086c-.15.529-.455.547-.679.045L12 14l6-8-8 6-8.054-2.685z"
-                          ></path>
-                        </svg>
-                      </div>
-                    </div>
-                    <span>Leave a Review</span>
-                  </button>
-                </>
-              )}
+              <button className="button" onClick={addFavoriteWithoutModal}>
+                {isFavorite === "added" ? (
+                  <span>❤️ Added to Favorite</span>
+                ) : (
+                  <span>🤍 Add to Favorite</span>
+                )}
+              </button>
+
+              {/* <button className="btn favorite">Leave a Review</button> */}
+              <button
+                class="button"
+                onClick={() => setIsReviewModalOpen(true)}
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  gap: "5px",
+                }}
+              >
+                <div class="svg-wrapper-1">
+                  <div class="svg-wrapper">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      width="24"
+                      height="24"
+                    >
+                      <path fill="none" d="M0 0h24v24H0z"></path>
+                      <path
+                        fill="currentColor"
+                        d="M1.946 9.315c-.522-.174-.527-.455.01-.634l19.087-6.362c.529-.176.832.12.684.638l-5.454 19.086c-.15.529-.455.547-.679.045L12 14l6-8-8 6-8.054-2.685z"
+                      ></path>
+                    </svg>
+                  </div>
+                </div>
+                <span>Leave a Review</span>
+              </button>
             </div>
           </div>
         </div>
